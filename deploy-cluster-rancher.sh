0343508efa9cced50c8708bc8b371fd5f6e5f501
dlvargas123 <66894336+dlvargas123@users.noreply.github.com>
2023-07-14 15:15:34 -0500

    Update deploy-cluster-rancher.sh

--- deploy-cluster-rancher.sh
+++ deploy-cluster-rancher.sh
@@ -39,7 +39,51 @@ sleep 10
 # Ejecutar el script exportar-kubeconfig.sh
 echo "Ejecutando exportar-kubeconfig.sh..."
 sleep 2
-curl -sSf https://raw.githubusercontent.com/dlvargas123/kubernetes-dlvargas/main/exportar-kubeconfig.sh | bash
+ruta_script="/etc/rancher/rke2/rke2.yaml"
+
+while [ ! -f "$ruta_script" ]; do
+    echo "El archivo $ruta_script no existe. Esperando..."
+    sleep 5  # Pausa de 5 segundos antes de verificar nuevamente
+done
+# El archivo existe, puedes ejecutar el script aquí
+ruta_script1="/etc/rancher/rke2/rke2.yaml"
+sleep 1
+# Leer el contenido del archivo script1
+contenido_script1=$(cat "$ruta_script1")
+sleep 1
+# Eliminar las líneas que contengan "certificate-authority-data"
+nuevo_contenido=$(echo "$contenido_script1" | grep -v "certificate-authority-data")
+sleep 1
+# Guardar el nuevo contenido en un archivo temporal
+archivo_temporal="/etc/rancher/rke2/rke2.yaml_bck2"
+echo "$nuevo_contenido" > "$archivo_temporal"
+sleep 1
+echo "Se ha eliminado la línea 'certificate-authority-data' del archivo script1."
+sleep 1
+ipextp=$(curl -s icanhazip.com)
+sleep 1
+ruta_archivo="/etc/rancher/rke2/rke2.yaml_bck2"
+ipextp=$(curl -s icanhazip.com) # Reemplaza "ip_del_servidor" por la IP externa correspondiente
+sleep 1
+# Leer el contenido del archivo
+contenido=$(cat "$ruta_archivo")
+sleep 1
+ipextp=$(curl -s icanhazip.com) # Reemplaza "ip_del_servidor" por la IP externa correspondiente
+# Cambiar el valor de server
+contenido_modificado=$(echo "$contenido" | sed "s|server: https://127.0.0.1:6443|server: https://$ipextp:6443|")
+sleep 1
+# Agregar la línea insecure-skip-tls-verify
+contenido_modificado=$(echo "$contenido_modificado" | sed "/server: https:\/\/$ipextp:6443/a \    insecure-skip-tls-verify: true")
+sleep 1
+# Guardar el contenido modificado en el archivo
+echo "$contenido_modificado" > "$ruta_archivo"
+sleep 1
+echo "Se ha realizado la modificación en el archivo $ruta_archivo."
+sleep 1
+# Copiar el archivo modificado a la ruta /root/.kube/config
+cp /etc/rancher/rke2/rke2.yaml /root/.kube/config
+echo "kubeconfig preparado"
+sleep 2
 # Verificar si se ejecutó correctamente
 if [ $? -eq 0 ]; then
   echo "El script exportar-kubeconfig.sh se ejecutó correctamente."
